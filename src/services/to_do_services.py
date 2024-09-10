d1783d73c7950a5a77644fbf45ea9449682c1b4b
ignaarguello <igna.arguello2110@hotmail.com>
2024-09-09 21:21:47 -0300

    Add: ultimo cambios

--- src/services/to_do_services.py
+++ src/services/to_do_services.py
@@ -4,7 +4,7 @@ from config.mongodb import mongo
 from bson import json_util, ObjectId
 
 
-# Service para crear tareas - (POST)
+# ? Service para crear tareas - (POST)
 def create_to_do_service():
     data = request.get_json()
     name = data.get("name", None)
@@ -26,21 +26,21 @@ def create_to_do_service():
         return "Invalid Payload", 400
 
 
-# Service para obtener todos los datos - (GET)
+# ? Service para obtener todos los datos - (GET)
 def get_all_to_do_service():
     data = mongo.db.todos.find()
     result = json_util.dumps(data)
     return Response(result, mimetype="application/json")
 
 
-# Service para obtener dato por ID
+# ? Service para obtener dato por ID - (GET <ID>)
 def get_to_do_for_id_service(id):
     data = mongo.db.todos.find_one({"_id": ObjectId(id)})
     result = json_util.dumps(data)
     return Response(result, mimetype="application/json")
 
 
-# Service para modificar dato - (Update)
+# ? Service para modificar dato - (Update)
 def update_to_do_service(id):
     data = request.get_json()
     if len(data) == 0:
@@ -52,7 +52,7 @@ def update_to_do_service(id):
         return "Todo not found", 404
 
 
-# Service para eliminar dato
+# ? Service para eliminar dato - (Delete)
 def delete_to_do_service(id):
     response = mongo.db.todos.delete_one({"_id": ObjectId(id)})
     if response.deleted_count >= 1:
